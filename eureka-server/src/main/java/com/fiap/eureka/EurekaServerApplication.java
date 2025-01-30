7cf4656d808ef564de99d9bd9f2f041aed7251ab
LuizRomao02 <luizromao7@outlook.com>
2025-01-30 15:22:58 -0300

    feat: adding docker

--- eureka-server/src/main/java/com/fiap/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/fiap/eureka/EurekaServerApplication.java
@@ -1,10 +1,13 @@
 package com.fiap.eureka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    SpringApplication.run(EurekaServerApplication.class, args);
   }
 }
