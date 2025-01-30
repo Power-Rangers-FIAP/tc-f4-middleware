b611ca9e457111ec46c47a83e5b1d9f3e4e10b9a
LuizRomao02 <luizromao7@outlook.com>
2025-01-30 14:45:34 -0300

    feat: creating the module eureka-server

--- eureka-server/src/main/java/com/fiap/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/fiap/eureka/EurekaServerApplication.java
@@ -1,5 +1,8 @@
 package com.fiap.eureka;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EurekaServerApplication {
   public static void main(String[] args) {
     System.out.println("Hello world!");
